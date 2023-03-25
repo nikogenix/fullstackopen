f5a725d3f3b68c718b041bb0ebdcffe611eeb758
justKara <nikogenix@gmail.com>
2023-03-25 02:18:56 +0200

    Updating part 1. Adding lessons to .gitignore

--- part01/course-information/src/App.js
+++ part01/course-information/src/App.js
@@ -13,31 +13,42 @@ const Part = (props) => {
 const Content = (props) => {
 	return (
 		<div>
-			<Part part={props.part[0]} exercises={props.exercises[0]} />
-			<Part part={props.part[1]} exercises={props.exercises[1]} />
-			<Part part={props.part[2]} exercises={props.exercises[2]} />
+			{props.parts.map((part) => (
+				<Part part={part.name} exercises={part.exercises} />
+			))}
 		</div>
 	);
 };
 
 const Total = (props) => {
-	return <p>Number of exercises {props.count}</p>;
+	const total = props.parts.reduce((acc, c) => c.exercises + acc, 0);
+	return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
-	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course} />
-			<Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-			<Total count={exercises1 + exercises2 + exercises3} />
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
