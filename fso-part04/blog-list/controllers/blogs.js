a3fd898d21bc484b74778d2fa3a1b3692ca34c35
justKara <nikogenix@gmail.com>
2023-04-11 05:07:12 +0300

    Adding another middleware for tokens

--- fso-part04/blog-list/controllers/blogs.js
+++ fso-part04/blog-list/controllers/blogs.js
@@ -11,11 +11,7 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
 	const body = request.body;
 
-	const decodedToken = jwt.verify(request.token, process.env.SECRET);
-	if (!decodedToken.id) {
-		return response.status(401).json({ error: "token invalid" });
-	}
-	const user = await User.findById(decodedToken.id);
+	const user = request.user;
 
 	const blog = new Blog({
 		title: body.title,
@@ -34,12 +30,7 @@ blogsRouter.post("/", async (request, response) => {
 blogsRouter.delete("/:id", async (request, response) => {
 	const blog = await Blog.findById(request.params.id);
 
-	const decodedToken = jwt.verify(request.token, process.env.SECRET);
-	if (!decodedToken.id) {
-		return response.status(401).json({ error: "token invalid" });
-	}
-
-	const user = await User.findById(decodedToken.id);
+	const user = request.user;
 
 	if (blog.author.toString() === user.id.toString()) {
 		await Blog.findByIdAndRemove(request.params.id);
