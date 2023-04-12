d50abd9caa4d60d8048831d902557b67c7e29431
justKara <nikogenix@gmail.com>
2023-04-12 05:22:34 +0300

    Adding login and submission functionality

--- fso-part05/blog-list-frontend/src/App.js
+++ fso-part05/blog-list-frontend/src/App.js
@@ -1,24 +1,182 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import "./index.css";
+
+const Notification = ({ message, error }) => {
+	if (message === null) {
+		return null;
+	}
+
+	return <span className={error ? "error" : "notification"}>{message}</span>;
+};
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([]);
+	const [newBlogTitle, setNewBlogTitle] = useState("");
+	const [newBlogAuthor, setNewBlogAuthor] = useState("");
+	const [newBlogUrl, setNewBlogUrl] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [user, setUser] = useState(null);
+	const [notification, setNotification] = useState([null, false]); // notification message; error: true/false
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+	useEffect(() => {
+		if (user) blogService.getAll().then((blogs) => setBlogs(blogs));
+	}, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem("blogAppUser");
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+			blogService.getAll().then((blogs) => setBlogs(blogs));
+		}
+	}, []);
 
-export default App
+	const handleLogin = async (event) => {
+		event.preventDefault();
+
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
+			window.localStorage.setItem("blogAppUser", JSON.stringify(user));
+			setUser(user);
+			setUsername("");
+			setPassword("");
+			blogService.getAll().then((blogs) => setBlogs(blogs));
+			setNotification(["correct credentials", false]);
+			setTimeout(() => {
+				setNotification([null, false]);
+			}, 5000);
+		} catch (exception) {
+			setNotification(["wrong credentials", true]);
+			setTimeout(() => {
+				setNotification([null, false]);
+			}, 5000);
+		}
+	};
+
+	const handleLogout = () => {
+		window.localStorage.removeItem("blogAppUser");
+		setUser(null);
+		setNotification(["logged out", false]);
+		setTimeout(() => {
+			setNotification([null, false]);
+		}, 5000);
+	};
+
+	const handleTitleChange = (event) => {
+		setNewBlogTitle(event.target.value);
+	};
+
+	const handleAuthorChange = (event) => {
+		setNewBlogAuthor(event.target.value);
+	};
+
+	const handleUrlChange = (event) => {
+		setNewBlogUrl(event.target.value);
+	};
+
+	const addBlog = (event) => {
+		event.preventDefault();
+		const blogObject = {
+			title: newBlogTitle,
+			author: newBlogAuthor,
+			url: newBlogUrl,
+		};
+
+		blogService
+			.create(blogObject)
+			.then((returnedBlog) => {
+				setBlogs(blogs.concat(returnedBlog));
+				setNewBlogTitle("");
+				setNewBlogUrl("");
+				setNotification(["new blog added successfully", false]);
+				setTimeout(() => {
+					setNotification([null, false]);
+				}, 5000);
+			})
+			.catch((exception) => {
+				setNotification(["submission failed; all fields are required", true]);
+				setTimeout(() => {
+					setNotification([null, false]);
+				}, 5000);
+			});
+	};
+
+	const loginForm = () => (
+		<form onSubmit={handleLogin}>
+			<div>
+				username
+				<input
+					type="text"
+					value={username}
+					name="Username"
+					onChange={({ target }) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				password
+				<input
+					type="password"
+					value={password}
+					name="Password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">login</button>
+		</form>
+	);
+
+	const blogForm = () => (
+		<div>
+			<h1>create new</h1>
+			<form onSubmit={addBlog}>
+				<div>
+					title
+					<input value={newBlogTitle} onChange={handleTitleChange} />
+				</div>
+				<div>
+					author
+					<input value={newBlogAuthor} onChange={handleAuthorChange} />
+				</div>
+				<div>
+					url
+					<input value={newBlogUrl} onChange={handleUrlChange} />
+				</div>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+
+	return (
+		<div>
+			<h1>welcome</h1>
+			<Notification message={notification[0]} error={notification[1]} />
+			{user === null && loginForm()}
+			{user && (
+				<div>
+					<div>
+						<p>
+							<b>logged in as {user.name}</b>
+						</p>
+						<button onClick={handleLogout}>logout</button>
+					</div>
+					<div>{blogForm()}</div>
+					<div>
+						<h1>blogs</h1>
+						{blogs.map((blog) => (
+							<Blog key={blog.id} blog={blog} />
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default App;
--- fso-part05/blog-list-frontend/src/services/blogs.js
+++ fso-part05/blog-list-frontend/src/services/blogs.js
@@ -1,10 +1,26 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+// using alternative way of checking the token from here:
+// https://blog.logrocket.com/using-axios-set-request-headers/
+
+axios.interceptors.request.use((config) => {
+	const userJSON = window.localStorage.getItem("blogAppUser");
+	const token = userJSON ? JSON.parse(userJSON)?.token : null;
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
+});
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+	const request = axios.get(baseUrl);
+	return request.then((response) => response.data);
+};
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+const create = async (newObject) => {
+	const response = await axios.post(baseUrl, newObject);
+	return response.data;
+};
+
+export default { getAll, create };
