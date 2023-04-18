308437b0168fa73adaad7b88587eda20452ab494
justKara <nikogenix@gmail.com>
2023-04-18 03:56:15 +0300

    Adding basic functionality and tests

--- fso-part06/unicafe-redux/src/index.js
+++ fso-part06/unicafe-redux/src/index.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import counterReducer from "./reducers/counterReducer";
 
-const store = createStore(reducer)
+const store = createStore(counterReducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+	const good = () => store.dispatch({ type: "GOOD" });
+	const ok = () => store.dispatch({ type: "OK" });
+	const bad = () => store.dispatch({ type: "BAD" });
+	const zero = () => store.dispatch({ type: "ZERO" });
 
-  return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<button onClick={good}>good</button>
+			<button onClick={ok}>ok</button>
+			<button onClick={bad}>bad</button>
+			<button onClick={zero}>reset stats</button>
+			<div>good {store.getState().good}</div>
+			<div>ok {store.getState().ok}</div>
+			<div>bad {store.getState().bad}</div>
+		</div>
+	);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+	root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
