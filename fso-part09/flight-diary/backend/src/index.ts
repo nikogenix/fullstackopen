207e33e8b59868b5cf9a051acec19246872993cc
nikogenix <nikogenix@gmail.com>
2023-05-23 05:05:01 +0300

    Adding CORS to fix backend

--- fso-part09/flight-diary/backend/src/index.ts
+++ fso-part09/flight-diary/backend/src/index.ts
@@ -1,17 +1,21 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+
+import diaryRouter from "./routes/diaries";
+
 const app = express();
-import diaryRouter from './routes/diaries';
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+app.get("/ping", (_req, res) => {
+	console.log("someone pinged here");
+	res.send("pong");
 });
 
-app.use('/api/diaries', diaryRouter);
+app.use("/api/diaries", diaryRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
